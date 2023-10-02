2214b300c75cdb029cde27a5d3529166e8dc23a0
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2023-10-02 11:10:12 -0300

    calcular costo

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -24,3 +24,15 @@ struct Dijkstra {
     vector<int> pacu;
     vector<Vertice> prev;
 };
+
+const int oo = 100;
+
+// Función para calcular el costo entre dos vértices
+int costo(const vector<Arista>& aristas, const Vertice& origen, const Vertice& destino) {
+    for (const auto& arista : aristas) {
+        if (arista.origen.nombre == origen.nombre && arista.destino.nombre == destino.nombre) {
+            return arista.peso;
+        }
+    }
+    return oo;
+}
