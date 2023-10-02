869266f62c3321fee1a9ad68cf99cc62daf19a31
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2023-10-02 12:01:08 -0300

    Inicializar pesos acumulados

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -43,6 +43,8 @@ int costo(const vector<Arista>& aristas, const Vertice& origen, const Vertice& d
 
 // Función para inicializar la lista de predecesores
 vector<Vertice> prevInit(const Vertice& v, const vector<Vertice>& vertices) {
+    // Se crea un vector que tiene el mismo tamaño que el número de vértices en el grafo 
+    // y se inizalizan todos los elementos del vector en {0}
     vector<Vertice> predecesores(vertices.size(), {0});
     //Aqui se usa transform que es el equivalente a map. Va a aplicar una operacion a cada elemento dentro del rango especificado.
     transform(vertices.begin(), vertices.end(), predecesores.begin(),
@@ -52,4 +54,24 @@ vector<Vertice> prevInit(const Vertice& v, const vector<Vertice>& vertices) {
                     // para indicar que no hay predecesor conocido para ese vértice.
                    [v](const Vertice& x) { return (x.nombre == v.nombre) ? x : Vertice{0}; });
     return predecesores;
+}
+
+// Función para inicializar la lista de pesos acumulados
+vector<int> acuInit(const vector<Arista>& aristas, const Vertice& v, const vector<Vertice>& vertices) {
+    // Se crea un vector que tiene el mismo tamaño que el número de vértices en el grafo 
+    // y se inizalizan todos los elementos del vector en {0}
+    vector<int> pesosAcumulados(vertices.size(), 0);
+    //Aqui se usa transform que es el equivalente a map. Va a aplicar una operacion a cada elemento dentro del rango especificado.
+    transform(vertices.begin(), vertices.end(), pesosAcumulados.begin(),
+                // La función lambda verifica si el vértice x es igual al vértice inicial v. 
+                // Si son iguales, significa que estamos en el vértice inicial, por lo que el peso acumulado es 0. 
+                // Si no son iguales, se utiliza la función costo para calcular el costo desde v hasta x
+                   [v, aristas](const Vertice& x) {
+                       if (x.nombre == v.nombre) {
+                           return 0;
+                       } else {
+                           return costo(aristas, v, x);
+                       }
+                   });
+    return pesosAcumulados;
 }
